b05aeb8717987b6b3f19b8d9b68e3cc809225ed0
felipeholanda2077 <felipe_holandafreitas@hotmail.com>
2022-02-18 19:18:22 -0300

    Em Andamento

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Hotsite.Models;
+using MySql.Data.MySqlClient;
+
+namespace Hotsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Interesse cad)
+        {
+            try
+            {
+                DatabaseService dbs = new DatabaseService();
+                dbs.CadastraInteresse(cad);
+            
+
+            } catch(Exception e) {
+            _logger.LogError("Erro Global: " + e.Message);
+            }
+
+
+
+            return View("Index", cad);
+        }
+
+    }
+}
